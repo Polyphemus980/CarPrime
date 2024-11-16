ac875fad31881f2a956fe985f1cbc833c39d89b9
Aleksander Bielicki <aleksanderbielicki2003@gmail.com>
2024-11-16 13:19:57 +0100

    test

--- CarPrime/Program.cs
+++ CarPrime/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSwaggerGen();
 var connectionString = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING") 
                        ?? builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseAzureSql(connectionString));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
