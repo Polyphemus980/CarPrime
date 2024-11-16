7682f6dcc2ae1135db5edd002819a73bc74b8e00
Aleksander Bielicki <aleksanderbielicki2003@gmail.com>
2024-11-16 01:08:56 +0100

    Test migration

---
+++ CarPrime/Migrations/20241116000545_TestMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarPrime.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TestField",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestField",
+                table: "Companies");
+        }
+    }
+}
