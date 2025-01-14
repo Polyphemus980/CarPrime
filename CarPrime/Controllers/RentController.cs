1d50a2e6cfb4dacaf662255d58deed9a131add73
xblmati <mraczynski01@gmail.com>
2025-01-14 08:41:07 +0100

    forgot one file

--- CarPrime/Controllers/RentController.cs
+++ CarPrime/Controllers/RentController.cs
@@ -14,6 +14,7 @@ public class RentController(
     IRentalService rentalService,
     ICustomerService customerService,
     ILogger<RentController> logger,
+    CarPrimeService carPrimeService,
     IBlobService blobService
 ) : ControllerBase
 {
